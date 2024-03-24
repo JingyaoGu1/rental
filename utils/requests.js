4d2129526be4a0f36d6d504b6a8c3941e67813fa
Jingyao Gu <43628019+JingyaoGu1@users.noreply.github.com>
2024-03-24 13:30:52 -0500

    each property page added

--- utils/requests.js
+++ utils/requests.js
@@ -1,4 +1,6 @@
 const apiDomian = process.env.NEXT_PUBLIC_API_DOMAIN || null
+
+// Fetch all properties
 async function fetchProperty() {
     try {
         // handle the case when the domain is not available yet
@@ -22,5 +24,28 @@ async function fetchProperty() {
     }
   }
 
+// Fetch single property
+async function fetchEachProperty(id) {
+  try {
+      // handle the case when the domain is not available yet
 
-export { fetchProperty };
+    if (!apiDomian) {
+      return null;
+    }
+      
+    const res = await fetch(`${apiDomian}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data")
+    }
+
+
+    return res.json();
+
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
+}
+
+export { fetchProperty, fetchEachProperty };
